c78cd086b75dd53f2b03f9947be0a5d992b24631
ReginaSadrieva <regina.sadrieva.pracovni@gmail.com>
2025-06-28 18:29:03 +0900

    servies proces added

---
+++ src/js/blocks/_prices.js
@@ -0,0 +1,27 @@
+let swiperInstance = null;
+
+function initSwiper() {
+  if (window.innerWidth < 768 && !swiperInstance) {
+    swiperInstance = new Swiper(".services_prices__list", {
+      slidesPerView: "auto",
+      spaceBetween: 16,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  } else if (window.innerWidth >= 768 && swiperInstance) {
+    swiperInstance.destroy();
+    swiperInstance = null;
+  }
+}
+
+function handleResize() {
+  initSwiper();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  initSwiper();
+});
+
+window.addEventListener("resize", handleResize);
--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,4 @@ import './blocks/_header';
 import './blocks/_menu';
 import './blocks/_brands';
 import './blocks/_types'
+import './blocks/_prices';
